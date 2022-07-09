ce33cc24449bf663c1d34c015b05f5c3d2f59f0e
Edward kataika <ekataika2002@gmail.com>
2022-07-09 18:31:46 +0200

    changed aapproach because of the negative remainder

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastd = abs(number) % 10
-if lastd > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number,lastd))
-elif lastd == 0:
-    print("Last digit of {} is {} and is 0".format(number,lastd))
-elif lastd < 6 and lastd != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number,lastd))
+if number < 0:
+    lastD = number % -10
+else:
+    lastD = number % 10
+print('Last digit of', number, 'is', lastD, end=' ')
+if lastD > 5:
+    print('and is greater than 5')
+elif lastD == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
+
