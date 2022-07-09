36ba3e8022b7cdb7f2a4df40e1a1821fc292f730
Edward kataika <ekataika2002@gmail.com>
2022-07-09 21:41:54 +0200

    more characters fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range (00,100):
+for number in range(0, 100):
     if number == 99:
-        print("{},\n".format(number))
+        print("{}".format(number))
     else:
-        print("{:02d},".format(number), end=" ")
+        print("{:02}".format(number), end=", ")
