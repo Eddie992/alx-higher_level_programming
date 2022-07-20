aa5f2ed7aef95edc3b7b5ea295ea68533acd367d
Edward kataika <ekataika2002@gmail.com>
2022-07-20 23:35:45 +0200

    correct output fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for fiz in range(1, 100):
-        if fiz % 3 == 0:
-            print("Fizz", end=" ")
-        elif fiz % 5 == 0:
-            print ("Buzz", end=" ")
-        elif fiz % 3 == 0 and fiz % 5 == 0:
-            print("FizzBuzz", end=" ")
-        else:
-            print("{}".format(fiz), end=" ")
-    return
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end='')
+        if i % 5 == 0:
+            print("Buzz", end='')
+        if i % 3 and i % 5:
+            print("{:d}".format(i), end='')
+        print(end=' ')
