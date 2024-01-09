f4de23c331a335732474a6ad41d15f9075d7881d
Eddie992 <ekataika2002@gmail.com>
2024-01-09 21:06:13 +0200

    bug fixes in the questions

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     import sys
+    result = 0
+    if (len(sys.argv) > 1):
+        for i in range(1, len(sys.argv)):
+            result += (int(sys.argv[i]))
+    print("{:d}".format(result))
 
-    value = 0
-    for i in range(len(sys.argv) - 1):
-        value = value + int(sys.argv[i + 1])
-    print("{}".format(value))
+# import sys
+# print("{:d}".format(sum(int(n) for n in sys.argv[1:])))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     import hidden_4
-    built = dir(hidden_4)
-    for name in built:
-        if name[:2] != "__":
-            print(name)
+    for i in dir(hidden_4):
+        if not i.startswith("__"):
+            print("{:s}".format(i))
