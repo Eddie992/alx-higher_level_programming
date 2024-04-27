a79f1d5102f25897b188433e7233a9e4cdce1c50
Edward Kataika <ekataika2002@gmail.com>
2024-04-27 15:37:19 -0700

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,10 +5,6 @@ sends a POST request to the passed URL with the email as a parameter,
 displays the body of the response (decoded in utf-8)
 
 """
-import sys
-import urllib.parse
-import urllib.request
-
 if __name__ == "__main__":
     import sys
     from urllib import request, error
